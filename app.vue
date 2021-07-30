4fb38517bd96a2bcad71167b16a12fc65351730c
sota-onozuka <sonta.afcot37@gmail.com>
2021-07-31 01:05:38 +0900

    単一ファイルにまとめた

--- app.vue
+++ app.vue
@@ -1,23 +1,39 @@
-Vue.component('todo-options', {
-  template: '\
-    <li>\
-      {{ title }}\
-      <button v-on:click="$emit(\'removetodo\')">Remove</button>\
-      <button v-on:click="$emit(\'seteditinfo\')">Edit</button>\
-      </li>\
-    ',
-  props: ['title']
-})
+<template>
+  <div id="app">
+    <form v-on:submit.prevent="addOrSet">
+      <label for="new-todo">Add a todo</label>
+      <input
+        v-model="newTodoText"
+        ref="editor"
+      >
+      <button>{{ changeButtonText }}</button>
+    </form>
+    <ul>
+      <li
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id">
+      {{todo.title}}
+        <span class="pull-right">
+          <button v-on:click="removeTodo(index)">Remove</button>
+          <button v-on:click="setEditInfo(index)">Edit</button>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
 
-new Vue({
+<script>
+
+export default {
   el: '#app',
-  data: {
+  data: function () {
+    return {
     newTodoText: '',
     editIndex: -1,
     todos: [],
-    nextTodoId: 0,
+    nextTodoId: 0
+    }
   },
-
   computed: {
     changeButtonText() {
         return this.editIndex === -1 ? 'add' : 'finish edition';
@@ -75,4 +91,29 @@ new Vue({
       this.editIndex = -1;
     },
   },
-})
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+#app {
+  max-width: 640px;
+  margin: 0 auto;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+button {
+  border: none;
+  border-radius: 20px;
+  line-height: 24px;
+  padding: 0 8px;
+  background: #0099e4;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
